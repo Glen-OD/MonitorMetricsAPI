0d9c156bf0d66f84c25718fa494f208566d6b925
40233306 <godonovan01@qub.ac.uk>
2021-12-10 02:22:57 +0000

    readding performance

--- monitorRoutes/index.js
+++ monitorRoutes/index.js
@@ -2,15 +2,17 @@ const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const { json } = require('express')
 
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 
 router.get('/', async (req, res) => {
+
+    
     try {
         t = performance.now()
-
         const params = new URLSearchParams({
             [API_KEY_NAME]: API_KEY_VALUE,
             ...url.parse(req.url, true).query
@@ -18,7 +20,7 @@ router.get('/', async (req, res) => {
 
         const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
         data = apiRes.body
-
+        
         t2 = performance.now()
         t3 = t2 - t
 
@@ -27,32 +29,33 @@ router.get('/', async (req, res) => {
             console.log(`REQUEST: ${API_BASE_URL}?${params}`)
         }
 
+        //if
         if(data.answer == 12)
         {
             if(data.string == "Contains 12 words")
             {
                 if(data.error == false)
                 {
-                    data = "URL 1 for WordCount working as expected. Time taken: "
+                    data = "URL 1 for WordCount working as expected. Time taken: " + t3.toFixed(2) + " milliseconds."
                 }
                 else{
                     dataError = data.error
-                    data = "error expected was: false received: " + dataError + ". Time taken: "
+                    data = "error expected was: false received: " + dataError + ". Time taken: " + t3.toFixed(2) + " milliseconds."
                 }
             }
             else{
                 dataString = data.string
-                data = "String expected was: Contains 12 words received: " + dataString + ". Time taken: "
+                data = "String expected was: Contains 12 words received: " + dataString + ". Time taken: " + t3.toFixed(2) + " milliseconds."
             }
         }
         else{
             dataAnswer = data.answer
-            data = "Answer expected was 12 received: " + dataAnswer + ". Time taken: "
+            data = "Answer expected was 12 received: " + dataAnswer + ". Time taken: " + t3.toFixed(2) + " milliseconds."
         }
-
-
-
+        //console.log(data)
         res.status(200).json(data)
+        //else - same as catch (error)
+
     } catch (error) {
         res.status(500).json({ error })
     }
